56c920fbc9cd1de5006c50c2fbebaec3afe2de60
WJoker <906336497@qq.com>
2018-10-25 18:10:50 +0800

    更新服务器配置

--- wsgi.py
+++ wsgi.py
@@ -8,3 +8,13 @@ import app
 
 sys.path.insert(0, abspath(dirname(__file__)))
 application = app.app
+
+
+"""
+配置 supervisor
+ln -s /var/www/essence/configs/essence.conf /etc/supervisor/conf.d/bbs.conf
+
+配置 Nginx 服务器
+ln -s /var/www/essence/configs/essence.nginx /etc/nginx/sites-enabled/essence
+配置重启 Nginx 之后，更新代码只需要重启 gunicorn
+"""
